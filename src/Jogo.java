2cd5f59787a91e313cb26be9eb3c301fa51b0ffc
gianellimurilo <murilogianelli22@gmail.com>
2026-08-13 17:31:57 -0300

    remove readme

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class Jogo {
+
+    Random random = new Random();
+
+    private int valor;
+
+    public Jogo() {
+        this.valor = random.nextInt(100)+1;
+    }
+    public int getValor() {
+        return valor;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean continuar = true;
+        int numeroCerto;
+        int palpite;
+
+        Jogo jogo = new Jogo();
+        numeroCerto = jogo.getValor();
+
+        while(continuar) {
+            System.out.println("Jogo da advinhação (1 a 100)");
+            System.out.println("");
+
+            System.out.println("Escolha o número do seu palpite: ");
+            palpite = scanner.nextInt();
+
+            int diferenca = Math.abs(palpite - numeroCerto);
+
+            if (diferenca > 50) {
+                System.out.println("Totalmente longe");
+            } else if (diferenca > 40) {
+                System.out.println("Muito longe");
+            } else if (diferenca > 30) {
+                System.out.println("Bem longe");
+            } else if (diferenca > 20) {
+                System.out.println("Longe");
+            } else if (diferenca > 15) {
+                System.out.println("Mais ou menos");
+            } else if (diferenca > 10) {
+                System.out.println("Aproximando");
+            } else if (diferenca > 8) {
+                System.out.println("Aproximando Mais");
+            } else if (diferenca > 6) {
+                System.out.println("Bem perto");
+            } else if (diferenca > 4) {
+                System.out.println("Muito perto");
+            } else if (diferenca > 2) {
+                System.out.println("Completamente perto");
+            } else if (diferenca > 1) {
+                System.out.println("Pertissimo");
+            }else if (diferenca == 1) {
+                System.out.println("Na trave");
+            } else if (diferenca == 0) {
+                System.out.println("ACERTOU!!!");
+                continuar = false;
+            }
+        }
+    }
 }
